734f6125499b8359ff93550aaa22cf5cac41f863
Marie <eigrads@hotmail.com>
2021-06-04 21:11:51 +0200

    add online course in link

--- commands/link.js
+++ commands/link.js
@@ -3,7 +3,16 @@ module.exports = (text) => {
     const firstWord = text.split(' ')[0];
     console.log(firstWord)
     const mapping = {
-        yt: "https://www.youtube.com/channel/UCVo4TMKRmdiMYHdMaPWbPNA"
+        //UE FR YT channel
+        uefr: "https://www.youtube.com/channel/UCVo4TMKRmdiMYHdMaPWbPNA",
+        //Gamedev Teacher channel
+        evan: "https://www.youtube.com/channel/UCyswDjUH1_gFIFJyu0sjJMg",
+        //The Cherno channel
+        cherno: "https://www.youtube.com/channel/UCQ-W1KE9EYfdxhL6S4twUNw",
+        //Unreal Engine channel
+        ue: "https://www.youtube.com/channel/UCBobmJyzsJ6Ll7UbfhI4iwQ",
+        //online course
+        "online-course": "https://www.unrealengine.com/en-US/onlinelearning-courses",
     }
 
     return mapping[firstWord];
