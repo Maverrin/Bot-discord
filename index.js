42df26ef720ba96eafbe8b2776c42951a437f663
EtienneDuv <etienne.duverney@viacesi.fr>
2021-06-04 18:39:37 +0200

    command starting with special character

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const commands = require('./commands');
 
+
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
 
 client.on('message', msg => {
+    if (msg.content[0] !== '!') return;
+
     const mapping = {
         ping: commands.pong,
     };
-    
-    if (!(msg.content in mapping)) return;
 
-    console.log('Message: ', msg.content);
+    const command = msg.content.split(' ')[0].substr(1);
+
+    if (!(command in mapping)) return;
 
     msg.reply(mapping[msg]);
 });
