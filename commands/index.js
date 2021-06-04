19cb8071ae84fce986ca829fb72cdb0e8dc2342e
EtienneDuv <etienne.duverney@viacesi.fr>
2021-06-04 17:47:39 +0200

    First command: ping

---
+++ commands/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    pong: require('./pong'),
+};
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const commands = require('./commands');
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-    }
+    const mapping = {
+        ping: commands.pong,
+    };
+    
+    if (!(msg.content in mapping)) return;
+
+    console.log('Message: ', msg.content);
+
+    msg.reply(mapping[msg]);
 });
 
 client.login(process.env.TOKEN);
