c1c9b5732e62a9419521bc9591173e198f485b79
EtienneDuv <etienne.duverney@bistudio.com>
2021-06-15 17:56:34 +0200

    Reformat a bit messages handler + move strings

--- src/handlers/message.js
+++ src/handlers/message.js
@@ -1,35 +1,24 @@
 const commands = require('../commands');
 const quotes = require('../../data/quotes.json');
 const {tryToSend} = require('../utils');
+const {helperMessage, errorMessage} = require('../../data/strings');
 
 // this variable is for the bot to not spam
-// the helperString message 
+// the helperMessage message 
 let canSendHelp = true;
 const helperTimeout = 15*1000;
+// const errorMessage = 
 
 module.exports = (client, msg) => {
+    const words = msg.content.split(' ');
+    const command = words.shift().substr(1);
+    const message = words.join(' ');
+  
     if (msg.author.bot) return;
-
-    // ============================
-    // VARIABLES
-    // ============================
-    
-    const helperString = 
-    {
-        embed: {
-            title      : 'Mauvaise commande. Voici la liste des commandes possibles:',
-            description: `**!say [text]** -  Fait dire votre texte au bot.\n
-            **!link [uefr | evan | cherno | ue | uol | a2a]**  -  Donne le lien vers les ressources prédéfinies.\n
-            **![${Object.keys(quotes).toString().replace(/,/g, ' | ')}]**  -  Fait dire une phrase sauvegardée aléatoire de cette personne.\n
-            **!add [messageID]**  -  Ajoute une phrase pour la commande ![Pseudo].\n
-            **!impaye**  -  Créé une annonce de recrutement non payé.\n
-            **!paye**  -  Créé une annonce de recrutement payé.\n
-            **!service**  -  Crée une annonce de recrutement pour proposer ses services (freelance).\n`
-        }
-    };
+    if (command.length === 0) return;
 
     // ----------------------------------
-    // SERVER MESSAGES HANDLING
+    // MESSAGES IN DISCORD SERVER
     // ----------------------------------
     if (msg.channel.type == 'text' && msg.content[0] == '!') {
         const mapping = {
@@ -38,17 +27,10 @@ module.exports = (client, msg) => {
             quote: (userName) => tryToSend(msg.channel, commands.quote(userName, msg)),
             add  : (messageId) => commands.add(client, messageId, msg)
                 .then(text => tryToSend(msg.channel, text))     
-                .catch(err => tryToSend(msg.channel, `${messageId} not found :/`))
-    
+                .catch(err => tryToSend(msg.channel, `\`${messageId}\` not found :/`))
         };
 
-        const words = msg.content.split(' ');
-    
-        const command = words.shift().substr(1);
-        const message = words.join(' ');
         const quotedPersons = Object.keys(quotes);
-
-        if (command.length === 0) return;
 
         msg.delete();
 
@@ -56,7 +38,7 @@ module.exports = (client, msg) => {
         if (!(command in mapping) && !quotedPersons.includes(command) && canSendHelp === true) {
             canSendHelp = false;
             setTimeout(() => canSendHelp = true, helperTimeout);
-            return tryToSend(msg.channel, helperString);
+            return tryToSend(msg.channel, helperMessage);
         }
 
         // in this context, the command is a pseudo
@@ -67,36 +49,26 @@ module.exports = (client, msg) => {
             if (command in mapping) return mapping[command](message);
         } catch (error) {
             console.log(error);
-            return tryToSend(msg.channel, ':bangbang: Error, please check logs ¯\\_(ツ)_/¯');
+            return tryToSend(msg.channel, errorMessage);
         }
     }
 
     // ----------------------------------
-    // DM MESSAGE HANDLING
+    // MESSAGE IN DM
     // ----------------------------------
-    if (msg.channel.type == 'dm')
-    {
+    if (msg.channel.type == 'dm') {
         const mapping = {
             paid     : (text) => tryToSend(msg.channel, commands.paid(text, msg.author)),
             unpaid   : (text) => tryToSend(msg.channel, commands.unpaid(text, msg.author)),
-            freelance: (userName) => tryToSend(msg.channel, commands.freelance(userName, msg)),
+            freelance: (text) => tryToSend(msg.channel, commands.freelance(text, msg.author)),
         };
-
-        //prevent bot using commands
-        if (msg.author.bot) return;
-
-        const words = msg.content.split(' ');
-        const command = words.shift().substr(1);
-        const message = words.join(' ');
-
-        if (command.length === 0) return;
 
         // Handle any bug in commands
         try {
             if (command in mapping) return mapping[command](message);
         } catch (error) {
             console.log(error);
-            return tryToSend(msg.channel, ':bangbang: Erreur! Demande de l\'aide a un membre du staff ¯\\_(ツ)_/¯');
+            return tryToSend(msg.channel, errorMessage);
         }
     }
 };
