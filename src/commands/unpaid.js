588a237f2992442f88c9007010cf03fbffafabc8
EtienneDuv <etienne.duverney@bistudio.com>
2021-06-17 17:28:23 +0200

    Basic options handling

--- src/commands/unpaid.js
+++ src/commands/unpaid.js
@@ -2,40 +2,71 @@ module.exports = async (msg, user) => {
     const dmChannel = msg.channel;
     const questions = [
         {
-            "question": "Ajoute un titre qui définit clairement ce que tu recherches",
+            step    : 'title',
+            question: 'Ajoute un titre qui définit clairement ce que tu recherches',
         },
         {
-            "question": "Ajoute une description détaillée du projet et ce dont tu as besoin",
+            step    : 'description',
+            question: 'Ajoute une description détaillée du projet et ce dont tu as besoin',
         },
         {
-            "question": "Comment peut on te contacter?",
+            step    : 'contact',
+            question: 'Comment peut on te contacter?',
         },
         {
-            "question": "Presque fini! Regarde ton poste et fais en sorte que ça soit juste.",
-            "1": "Envoyer mon poste",
-            "2": "Recommencer"
+            step    : 'finish',
+            question: 'Presque fini! Regarde ton poste et fais en sorte que ça soit juste.',
+            options : [
+                '1. Envoyer mon poste', 
+                '2. Recommencer',
+            ] 
         }
-    ]
+    ];
 
-    const applying = [];
+    const answers = [];
+
     console.log(`${msg.author.tag} a commencé a postuler.`);
 
     for (let i = 0, cancel = false; i < questions.length && cancel === false; i++) {
         await dmChannel.send(questions[i].question);
-        console.log(questions[i].question);
-        try {
-            const collected = await dmChannel.awaitMessages(m => m.author.id === msg.author.id, { max: 1, time: 5 * 60 * 1000, errors: ['time'] });
 
-            applying.push(collected.last().content);
-            console.log(applying[i]);
-            if (collected.last().content.toLowerCase() === '!stop') {
+        try {
+            const hasOptions = 'options' in questions[i];
+
+            // Display options, if there are some
+            if (hasOptions) {
+                for (const option of questions[i].options) {
+                    await dmChannel.send(option);
+                }
+            }
+
+            // Fetch message
+            const collected = await dmChannel.awaitMessages(m => m.channel.type === 'dm', {max: 1, time: 5 * 60 * 1000, errors: ['time']});
+            const msgContent = collected.last().content.trim();
+            
+            // !stop command
+            // possible !edit command to change precedent answer
+            if (msgContent.toLowerCase() === '!stop') {
                 await dmChannel.send('**Application arrêté.**');
                 console.log(`${msg.author.tag} a stopper la création d'une annonce.`);
                 cancel = true;
             }
 
-            // Last question
-            if (i === questions.length - 1) optionChosen = collected.last().content;
+            // Check if content is the number of an option
+            let selectedOption;
+            if (hasOptions) {
+                if (msgContent.length > 1 || /[1-9]/.test(msgContent[0]) && (msgContent > questions[i].options.length)) {
+                    await dmChannel.send('**Veuillez tapper le numéro de l\'option choisie**');
+                    i--;
+                    continue;
+                }
+                selectedOption = msgContent;
+            }
+
+            // Save response
+            answers.push({[questions[i].step]: msgContent});
+            console.log(answers[i]);
+
         } catch (error) {
             await dmChannel.send('**L\'annonce a été mis en arrêt suite a un délai trop long.**');
             cancel = true;
@@ -44,4 +75,6 @@ module.exports = async (msg, user) => {
     }
 
     console.log(`${msg.author.tag} a fini de créer son annonce.`);
+
+    // utils.answersToEmber(answers)
 };
