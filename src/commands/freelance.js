a96346f512b7cd3b7e89c889c4a737de483c4a17
Marie <eigrads@hotmail.com>
2021-06-18 16:11:21 +0200

    option freelance

--- src/commands/freelance.js
+++ src/commands/freelance.js
@@ -1,38 +1,108 @@
-module.exports = async (msg, user) => {
+const { tryToSendChannelId, advertToEmbed } = require('../utils');
+
+module.exports = async (client, msg, user) => {
     const dmChannel = msg.channel;
     const questions = [
-        '**Faites-vous une annonce pour un freelancer individuel ou un studio ?\n',
-        '**Ajoute une description détaillée du projet et ce dont tu as besoins.**',
-        '**Comment peut-on te contacter ?**',
-        '**C\'est presque fini! Regarde ton message afin de vérifier qu\'il soit bon.**',
-        '**Message qui change en fonction d\'une réponse (choisi une option : `1` ou `2`)**',
+        {
+            step: 'title',
+            question: '**Faites-vous une annonce pour un freelancer individuel ou un studio ?**',
+            options: [
+                '1. **Freelance**',
+                '2. **Studio**',
+            ],
+            one: "**Quel est ton nom ?**",
+            two: "**Quel est le nom de ton studio ?**"
+        },
+        {
+            step: 'description',
+            question: '**Quel est la liste des services que tu proposes ? (Max 1024 lettres)**',
+        },
+        {
+            step: 'url',
+            question: '**Entrez l\'url de votre site portfolio (URL requis)**\nPar exemple: https://monsuperportfolio.com/',
+        },
+        {
+            step: 'contact',
+            question: '**Comment les clients potentiels peuvent-ils vous contacter ? (Max 1024 lettres)**',
+        },
+        {
+            step: 'finish',
+            question: '**Presque fini! Regarde ton poste et fais en sorte que ça soit juste.**', // + message example
+            options: [
+                '1. **Envoyer mon poste**',
+                '2. **Recommencer**',
+            ]
+        }
     ];
 
-    let optionChosen;
-    const questionsOption1 = ['1', '2'];
-    const questionsOption2 = ['3', '4'];
-
-    const applying = {};
+    const answers = {};
 
     console.log(`${msg.author.tag} a commencé a postuler.`);
 
     for (let i = 0, cancel = false; i < questions.length && cancel === false; i++) {
-        await dmChannel.send(questions[i]);
-        try {
-            const collected = await dmChannel.awaitMessages(m => m.author.id === msg.author.id, { max: 1, time: 5 * 60 * 1000, errors: ['time'] });
+        let questionMsg = questions[i].question;
+        let questionMsgOne = questions[i].one;
+        let questionMsgTwo = questions[i].two;
 
-            if (collected.last().content.toLowerCase() === '!stop') {
+        // Display options, if there are some
+        const hasOptions = 'options' in questions[i];
+        if (hasOptions) {
+            for (const option of questions[i].options) {
+                questionMsg += '\n' + option;
+            }
+        }
+        await dmChannel.send(questionMsg);
+        //if (questions[i].step === 'finish') await dmChannel.send(advertToEmbed(answers));
+
+        try {
+            // Fetch message
+            const collected = await dmChannel.awaitMessages(m => m.channel.type === 'dm', { max: 1, time: 5 * 60 * 1000, errors: ['time'] });
+            const msgContent = collected.last().content.trim();
+
+            // !stop command
+            // possible !edit command to change precedent answer
+            if (msgContent.toLowerCase() === '!stop') {
                 await dmChannel.send('**Application arrêté.**');
                 console.log(`${msg.author.tag} a stopper la création d'une annonce.`);
                 cancel = true;
             }
 
-            // Last question
-            if (i === questions.length - 1) optionChosen = collected.last().content;
+            // Check if content is the number of an option
+            if (hasOptions) {
+                const msg = await dmChannel.awaitMessages(m => m.channel.type === 'dm', { max: 1, time: 5 * 60 * 1000, errors: ['time'] });
+                const msgContentOption = msg.last().content.trim();
+                if (msgContent.length > 1 || !/[1-9]/.test(msgContent[0]) || (msgContent > questions[i].options.length)) {
+                    await dmChannel.send('**Veuillez tapper le numéro de l\'option choisie**');
+                    i--;
+                    continue;
+                }
+                if (msgContentOption == 1) {
+                    console.log("Choisi l'option 1")
+                    await dmChannel.send(questionMsgOne);
+                }
+                else if (msgContentOption == 2) {
+                    console.log("Choisi l'option 2")
+                    await dmChannel.send(questionMsgTwo);
+                }
+            }
+
+            // Save response
+            answers[questions[i].step] = msgContent;
+            console.log(`${msg.author.tag} - ${questions[i].step}: ${msgContent}`);
+
         } catch (error) {
+            console.log(error);
             await dmChannel.send('**L\'annonce a été mis en arrêt suite a un délai trop long.**');
             cancel = true;
             console.log(`${msg.author.tag} : délai trop long.`);
+        }
+
+        // Send advert
+        if (answers.finish === 1) {
+            tryToSendChannelId(client, process.env.ADVERT_CHANNEL_ID, advertToEmbed(answers));
+        }
+        else if (answers.finish === 2) {
+            i = -1;
         }
     }
 
